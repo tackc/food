d8b7681d38345019a1cf2497076849e5f0945573
Chris Tack <chris_tack@mac.com>
2020-08-09 21:18:56 -0700

    Add optional second argument

--- App.js
+++ App.js
@@ -3,4 +3,11 @@ import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator({
   Search: SearchScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Restaurant Search'
+  }
 });
+
+export default createAppContainer(navigator);
