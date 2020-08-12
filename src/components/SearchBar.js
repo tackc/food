dca0e6490ef9438544c00f58c5fd9c67458cfe15
Chris Tack <chris_tack@mac.com>
2020-08-11 21:48:59 -0700

    Convert input type

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
       <Feather name="search" size={30} />
-      <Text>Search Bar</Text>
+      <TextInput>Search Bar</TextInput>
     </View>
   );
 };
