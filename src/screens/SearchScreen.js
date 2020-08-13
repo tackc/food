13387d852c32f124dad3b265ba5308f4b3740e31
Chris Tack <chris_tack@mac.com>
2020-08-12 21:35:25 -0700

    Pass prop to child

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,7 +7,7 @@ const SearchScreen = () => {
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} onTermSubmit={() => console.log('term was submitted')} />
       <Text>Search Screen</Text>
     </View>
   );
