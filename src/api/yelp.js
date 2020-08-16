9f7687b490b3a3e614f54f9c92cce6b1d636c784
Chris Tack <chris_tack@mac.com>
2020-08-15 22:33:53 -0700

    import api key

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { YELP_API_KEY } from 'babel-dotenv';
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    // Authorization: `Bearer ${YELP_API_KEY}`
+    Authorization: `Bearer ${YELP_API_KEY}`
   }
 });
