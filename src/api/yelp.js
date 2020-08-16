7182fcb899cc1de8c64c92ea94f2cf70ae2fb1fa
Chris Tack <chris_tack@mac.com>
2020-08-15 22:29:42 -0700

    Remove reference to env

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { YELP_API_KEY } from 'react-native-dotenv';
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
-    Authorization: `Bearer ${YELP_API_KEY}`
+    // Authorization: `Bearer ${YELP_API_KEY}`
   }
 });
