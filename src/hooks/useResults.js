04adf1be80639e12e996ea150ee596e341c55159
Chris Tack <chris_tack@mac.com>
2020-08-16 22:40:11 -0700

    Extract api related code

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -2,5 +2,29 @@ import { useEffect, useState } from 'react';
 import yelp from '../api/yelp';
 
 export default () => {
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState([]);
 
+const searchApi = async (searchTerm) => {
+  try {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: searchTerm,
+        location: 'seattle'
+      }
+    });
+    setResults(response.data.businesses);
+  } catch (err) {
+    setErrorMessage('Something went wrong :(');
+  }
+};
+
+  // // Call searchApi when component is first rendered. BAD CODE
+  // searchApi('pasta'); this will create an infinite loop
+
+  // Passing empty array as second argument tells react to only run this once
+  useEffect(() => {
+    searchApi('pasta');
+  }, []);
 };
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,31 +5,6 @@ import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState([]);
-
-const searchApi = async (searchTerm) => {
-  try {
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term: searchTerm,
-        location: 'seattle'
-      }
-    });
-    setResults(response.data.businesses);
-  } catch (err) {
-    setErrorMessage('Something went wrong :(');
-  }
-};
-
-  // // Call searchApi when component is first rendered. BAD CODE
-  // searchApi('pasta'); this will create an infinite loop
-
-  // Passing empty array as second argument tells react to only run this once
-  useEffect(() => {
-    searchApi('pasta');
-  }, []);
 
   return (
     <View>
