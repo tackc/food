66dee3d6d7e9235548b26e67280786c82844538c
Chris Tack <chris_tack@mac.com>
2020-08-16 22:43:29 -0700

    import useResults

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
