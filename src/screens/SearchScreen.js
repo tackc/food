8591c8f510deac54b8f30460f09d9f5a8045d8d0
Chris Tack <chris_tack@mac.com>
2020-08-16 22:45:01 -0700

    create variables

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,6 +6,7 @@ import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  const [searchApi, results, errorMessage] = useResults();
 
   return (
     <View>
