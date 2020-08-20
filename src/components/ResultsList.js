a0069983ed0804ffec4e433f2143b3bc0d6a67dd
Chris Tack <chris_tack@mac.com>
2020-08-19 21:54:06 -0700

    Add import

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ResultsList = ({ title, results }) => {
   return (
