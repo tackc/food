1ce04f55d69d13c95158eb978b944604c0ab7851
Chris Tack <chris_tack@mac.com>
2020-08-19 21:51:58 -0700

    Create helper function to filter results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,14 +8,21 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
 
+const filterResultsByPrice = (price) => {
+  // price will equal '$' || '$$' || '$$$'
+  return results.filter(result => {
+    return result.price === price;
+  });
+};
+
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList title="Cheap" />
-      <ResultsList title="Mid-Range" />
-      <ResultsList title="Expensive" />
+      <ResultsList results={filterResultsByPrice('$')} title="Cheap" />
+      <ResultsList results={filterResultsByPrice('$$')} title="Mid-Range" />
+      <ResultsList results={filterResultsByPrice('$$$')} title="Expensive" />
     </View>
   );
 };
