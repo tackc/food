5fc77e4903bed69a1d25a2f6928ac7a1a6e78a35
Chris Tack <chris_tack@mac.com>
2020-08-21 22:25:40 -0700

    Add import

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -20,9 +20,11 @@ const filterResultsByPrice = (price) => {
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title="Cheap" />
-      <ResultsList results={filterResultsByPrice('$$')} title="Mid-Range" />
-      <ResultsList results={filterResultsByPrice('$$$')} title="Expensive" />
+      <ScrollView>
+        <ResultsList results={filterResultsByPrice('$')} title="Cheap" />
+        <ResultsList results={filterResultsByPrice('$$')} title="Mid-Range" />
+        <ResultsList results={filterResultsByPrice('$$$')} title="Expensive" />
+      </ScrollView>
     </View>
   );
 };
