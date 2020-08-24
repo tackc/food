f3d05d029495a72fd7f3c6d7b5b8e247653cfdc2
Chris Tack <chris_tack@mac.com>
2020-08-23 22:34:56 -0700

    Import

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results, navigation }) => {
