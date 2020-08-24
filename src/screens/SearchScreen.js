9bcf16028a78c5eb37fd074a18866e8a7a56a778
Chris Tack <chris_tack@mac.com>
2020-08-23 22:11:56 -0700

    Replace View element to fit content

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -16,7 +16,7 @@ const filterResultsByPrice = (price) => {
 };
 
   return (
-    <View style={{ flex: 1 }}>
+    <>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={() => searchApi(term)} />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
@@ -25,7 +25,7 @@ const filterResultsByPrice = (price) => {
         <ResultsList results={filterResultsByPrice('$$')} title="Mid-Range" />
         <ResultsList results={filterResultsByPrice('$$$')} title="Expensive" />
       </ScrollView>
-    </View>
+    </>
   );
 };
 
