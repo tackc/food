1bb88dcea43fa7604d401c73ee3ee53c28ead464
Chris Tack <chris_tack@mac.com>
2020-08-25 21:17:59 -0700

    Display and style images

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -21,10 +21,22 @@ if (!result) {
   return (
     <View>
       <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
 export default ResultsShowScreen;
