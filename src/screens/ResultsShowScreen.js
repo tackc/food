569f2aca461495e6eccfacf90694aa3a5e15cfcf
Chris Tack <chris_tack@mac.com>
2020-08-25 21:14:22 -0700

    Import FlatList

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
